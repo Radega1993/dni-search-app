c8705fb46d339a7bb7f4fc7a86f1b7cd94a94993
Raul de Arriba <rauldearriba@gmail.com>
2024-09-03 18:03:05 +0200

    aviso

--- src/components/DniSearch.js
+++ src/components/DniSearch.js
@@ -35,13 +35,16 @@ function DniSearch({ playersData }) {
           required
           className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <button 
+        <button
           type="submit"
           className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
         >
           Buscar
         </button>
       </form>
+
+      <h3 className="text-xl font-bold mb-6 text-center text-gray-800">Recorda que els entrenaments comencen aquesta primera setmana de setembre!!</h3>
+
     </div>
   );
 }
